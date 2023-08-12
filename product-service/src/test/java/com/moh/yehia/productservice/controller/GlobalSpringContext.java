c673170627b7c2ba56b8e36bd7c5da14d24000ac
Mohammed Yehia <mohammedyehia99@gmail.com>
2023-08-12 14:08:41 +0300

    adding spring-boot-admin-server for managing the actuator metrics & adding spring-boot-admin-client for all other microservices

--- product-service/src/test/java/com/moh/yehia/productservice/controller/GlobalSpringContext.java
+++ product-service/src/test/java/com/moh/yehia/productservice/controller/GlobalSpringContext.java
@@ -1,26 +1,26 @@
-package com.moh.yehia.productservice.controller;
-
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.util.Locale;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@Testcontainers
-public abstract class GlobalSpringContext {
-    @Autowired
-    MockMvc mockMvc;
-
-    static Faker faker;
-
-    @BeforeAll
-    static void initializeFaker() {
-        faker = new Faker(Locale.ENGLISH);
-    }
-}
+//package com.moh.yehia.productservice.controller;
+//
+//import com.github.javafaker.Faker;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.testcontainers.junit.jupiter.Testcontainers;
+//
+//import java.util.Locale;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//@Testcontainers
+//public abstract class GlobalSpringContext {
+//    @Autowired
+//    MockMvc mockMvc;
+//
+//    static Faker faker;
+//
+//    @BeforeAll
+//    static void initializeFaker() {
+//        faker = new Faker(Locale.ENGLISH);
+//    }
+//}
