0017a959c6c9ce3385bdd073c3993c89d2c9e8ce
mohyehia <mohammedyehia99@gmail.com>
2024-09-19 18:48:47 +0300

    refactor & organize imports

--- order-service/src/main/java/com/moh/yehia/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/moh/yehia/orderservice/controller/OrderController.java
@@ -7,14 +7,13 @@ import com.moh.yehia.orderservice.model.response.PlaceOrderResponse;
 import com.moh.yehia.orderservice.service.design.NotificationService;
 import com.moh.yehia.orderservice.service.design.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/orders")
